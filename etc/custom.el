0831b04d86446b42bf8fd1c0c508db2b493d691f
Jean-Nicolas Lagneau <jlagneau@users.noreply.github.com>
2025-07-08 00:05:00 +0200

    Add magit configuration to make diff highlights more readable.

--- etc/custom.el
+++ etc/custom.el
@@ -9,4 +9,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(custom-state ((t (:background "#b6e63e" :foreground "black"))))
+ '(magit-diff-added-highlight ((t (:extend t :background "#9ac334" :foreground "black" :weight bold)))))
