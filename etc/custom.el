21ecd2c7e7a4b320a4c67961b38e793409828455
Jean-Nicolas Lagneau <jlagneau@users.noreply.github.com>
2025-07-08 23:01:45 +0200

    Dired directory custom color

--- etc/custom.el
+++ etc/custom.el
@@ -10,4 +10,5 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-state ((t (:background "#b6e63e" :foreground "black"))))
+ '(dired-directory ((t (:foreground "deep sky blue"))))
  '(magit-diff-added-highlight ((t (:extend t :background "#9ac334" :foreground "black" :weight bold)))))
