122ed684f79bdf0f57cbcc75ad5d365a9db75717
Jean-Nicolas Lagneau <jlagneau@users.noreply.github.com>
2025-07-10 21:54:36 +0200

    Move the font selection to  custom.el

--- etc/custom.el
+++ etc/custom.el
@@ -9,6 +9,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit default :height 110 :family "Fira Code"))))
  '(custom-state ((t (:background "#b6e63e" :foreground "black"))))
  '(dired-directory ((t (:foreground "deep sky blue"))))
- '(magit-diff-added-highlight ((t (:extend t :background "#9ac334" :foreground "black" :weight bold)))))
+ '(magit-diff-added-highlight ((t (:extend t :background "#9ac334" :foreground "black" :weight bold))))
+ '(variable-pitch ((t (:height 110 :family "Noto Sans")))))
