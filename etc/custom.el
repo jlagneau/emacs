5e8b1622245bebcaaa427ea5ab0b4041c7354fec
Jean-Nicolas Lagneau <jlagneau@users.noreply.github.com>
2025-07-11 22:40:07 +0200

    Save customizations

--- etc/custom.el
+++ etc/custom.el
@@ -3,7 +3,14 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(git-gutter:added-sign "+")
+ '(git-gutter:deleted-sign "-")
+ '(git-gutter:modified-sign "=")
+ '(global-prettify-symbols-mode t)
+ '(indent-tabs-mode nil)
+ '(org-fold-catch-invisible-edits 'show-and-error nil nil "Customized with use-package org")
+ '(prettify-symbols-unprettify-at-point 'right-edge)
+ '(tab-width 4))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
